275b83171b0223200bf1000060f99a9cbbbf4966
kt-imimr <kt@imimr.net>
2024-03-18 17:59:06 +0800

    doing extract_filter fn

--- app.py
+++ app.py
@@ -1,21 +1,82 @@
 import re
 from flask import Flask, render_template, request
+from search import Search
 
 app = Flask(__name__)
-
+es = Search()
 
 @app.get('/')
 def index():
     return render_template('index.html')
 
-
 @app.post('/')
 def handle_search():
     query = request.form.get('query', '')
-    return render_template(
-        'index.html', query=query, results=[], from_=0, total=0)
+    filters, parsed_query = extract_filters(query)
+    from_ = request.form.get('from_', type=int, default=0)
+
+    results = es.search(
+        query={
+            'bool': {
+                'must': {
+                    'multi_match': {
+                        'query': parsed_query,
+                        'fields': ['name', 'summary', 'content'],
+                    }
+                },
+                **filters
+            }
+        },
+        size=5,
+        from_=from_
+    )
+    return render_template('index.html', results=results['hits']['hits'],
+                           query=query, from_=from_,
+                           total=results['hits']['total']['value'])
+
 
 
 @app.get('/document/<id>')
 def get_document(id):
-    return 'Document not found'
+    document = es.retrieve_document(id)
+    title = document['_source']['name']
+    paragraphs = document['_source']['content'].split('\n')
+    return render_template('document.html', title=title, paragraphs=paragraphs)
+
+def extract_filters(query):
+    filters = []
+
+    filter_regex = r'category:([^\s]+)\s*'
+    m = re.search(filter_regex, query)
+    if m:
+        filters.append({
+            'term': {
+                'category.keyword': {
+                    'value': m.group(1)
+                }
+            },
+        })
+        query = re.sub(filter_regex, '', query).strip()
+
+    filter_regex = r'year:([^\s]+)\s*'
+    m = re.search(filter_regex, query)
+    if m:
+        filters.append({
+            'range': {
+                'updated_at': {
+                    'gte': f'{m.group(1)}||/y',
+                    'lte': f'{m.group(1)}||/y',
+                }
+            },
+        })
+        query = re.sub(filter_regex, '', query).strip()
+
+    return {'filter': filters}, query
+
+
+@app.cli.command()
+def reindex():
+    """Regenerate the Elasticsearch index."""
+    response = es.reindex()
+    print(f'Index with {len(response["items"])} documents created '
+          f'in {response["took"]} milliseconds.')
