e1957c8065889f9a3507dd39d2f7308d4726e40f
kt-imimr <kt@imimr.net>
2024-04-12 18:24:22 +0800

    added chi detector

--- service/lang_detector.py
+++ service/lang_detector.py
@@ -22,6 +22,9 @@
 #     return language_setting
 
 
-# def detect_lang(text):
-#     return Detector(text).languages
+import re
+
+
+def detect_lang(text):
+    return 'chinese' if re.search(u'[\u4e00-\u9fff]', text) else 'english'
 
